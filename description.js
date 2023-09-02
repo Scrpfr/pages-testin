76ddedf059a1a05e90af562a6364343dcf21c728
Scrpfr <143892422+Scrpfr@users.noreply.github.com>
2023-09-02 20:27:46 +0500

    Add files via upload

---
+++ description.js
@@ -0,0 +1,9 @@
+const $board = document.getElementById('description');
+
+let description = `<p>
+Актуальные купоны можно найти во вкладке "Орбита игр".</br>
+Ввести купон можно во вкладке "Купоны".</br>
+По всем гильд вопросам к Majere.</br>
+</p>`;
+
+$board.insertAdjacentHTML('beforeend', description);
