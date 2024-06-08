59fdc975b256be91bbf78a2761bb134d19b036e5
Scrpfr <143892422+Scrpfr@users.noreply.github.com>
2024-06-09 00:39:20 +0500

    Delete description.js

--- description.js
+++
@@ -1,9 +0,0 @@
-const $board = document.getElementById('description');
-
-let description = `<p>
-Актуальные купоны можно найти во вкладке "Орбита игр".</br>
-Ввести купон можно во вкладке "Купоны".</br>
-По всем гильд вопросам к Majere.</br>
-</p>`;
-
-$board.insertAdjacentHTML('beforeend', description);
